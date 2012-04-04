2682ce414bb135b4df050ce19f5f605661bb3a96
Rob Sparrow <robsparrow@gmail.com>
2012-04-04 16:21:15 -0400

    Rewrote the various algorithms method for loading and transforming images.

--- diffevolution.m
+++ diffevolution.m
@@ -16,28 +16,16 @@ original = imread('test.png');
 % Setup transformations
 scale = 0.7; %70% of original image size
 theta = 30; %30 degree rotation counterclockwise
-
-%Setup a transformation matrix with x and y cooridnate translation
 yTrans = 20; %y translation defined in pixels
 xTrans = 20; %x translation defined in pixels
-translationMatrix = zeros(3,3);
-translationMatrix(1,1)=1;
-translationMatrix(2,2)=1;
-translationMatrix(3,3)=1;
-translationMatrix(3,1)=xTrans;
-translationMatrix(3,2)=yTrams;
 
 %Apply all transformations to the image
-tformTranslate = maketform('affine',translationMatrix);
-[translated xdata ydata]= imtransform(original, tformTranslate);
-scaled = imresize(translated, scale);
-rotated = imrotate(scaled,theta);
-distorted = rotated;
+[distorted, sample, xdata,ydata] = imretrieve(original, xTrans, yTrans, scale, theta);
 
 %Show original and transformed images for comparison, then an overlay
 figure, imshow(original);
 title('Original');
-figure, imshow(distorted);
-title('Distorted');
+figure, imshow(sample);
+title('Sample Distortion');
 imshowpair(original, distorted,'Scaling','joint');
 title('Overlay');
