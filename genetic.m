018cda4478627fb12b0c878d0c6199a39e20a1df
Rob Sparrow <robsparrow@gmail.com>
2012-04-10 20:30:25 -0400

    Misc. changes.

--- genetic.m
+++ genetic.m
@@ -1,59 +1,3 @@
-% clear all; clc;
-% 
-% %===============================================================================
-% % Title: Evolutionary Image Registration for Mosaicing of Remotely Sensed Images
-% % Author: Rob Sparrow, Systes Design Engineering, University of Waterloo
-% % April 4th, 2012
-% %===============================================================================
-%  
-% %-------------------------------------------------------------------------------
-% % Part 3: Genetic Algorithms
-% %-------------------------------------------------------------------------------
-% 
-% % Load original image
-% targetFolder = 'Samples';
-% original = '4.png';
-% original = strcat(targetFolder, '\', original);
-% original = imread(original);
-% original = rgb2gray(original);
-% 
-% % Setup transformations
-% scale = 1; % % of original image size
-% theta = 30; % degree rotation counterclockwise
-% xTrans = 0; %x translation defined in pixels
-% yTrans = 0; %y translation defined in pixels
-% 
-% %Apply all transformations to the image
-% % [distorted, sample, xdata,ydata] = imretrieve(original, xTrans, yTrans, scale, theta);
-% distorted = im_rst(original, scale, theta, xTrans, yTrans);
-% 
-% %Save images, which will be accessed by the GA
-% targetFolder = 'Test Data';
-% image = 'model4.png';
-% image = strcat(targetFolder, '\', image);
-% imwrite(original, image, 'png');
-% image = 'scene4.png';
-% image = strcat(targetFolder, '\', image);
-% imwrite(distorted, image, 'png');
-% 
-% %Show original and transformed images for comparison
-% % figure(1),subplot(2,1,1),imshow(original),title('Model');
-% % subplot(2,1,2),imshow(distorted),title('Scene');
-% 
-% %Specify options related to the performance of the GA
-% options=gaoptimset;
-% %Population size is 20 by default
-% options = gaoptimset('PopulationSize', 20);
-% 
-% %Run the GA
-% [x fval reason] = ga(@genetic_function, 4, options);
-% 
-% %Show the registered scene image based on the GA
-% [registered, sample, xdata, ydata] = imretrieve(distorted, x(3), x(4), x(1), x(2));
-% figure(1),subplot(2,1,1),imshow(original),title('Model');
-% subplot(2,1,2),imshow(distorted),title('Scene');
-% subplot(2,1,3),imshow(registered),title('Registered Scene');
-
 clear all; clc;
 
 %===============================================================================
@@ -98,14 +42,13 @@ imwrite(distorted, image, 'png');
 
 %Specify options related to the performance of the GA
 options=gaoptimset;
-%Population size is 20 by default
-options = gaoptimset('PopulationSize', 20);
+options = gaoptimset('Generations', 2);
 
 %Run the GA
-[x fval reason] = ga(@genetic_function, 1, options);
+[x fval reason] = ga(@genetic_function, 2, options);
 
 %Show the registered scene image based on the GA
-[registered, sample, xdata, ydata] = imretrieve(distorted, 0, 0, 0, x(1));
+registered = im_rst(distorted, 1, x(1), 0, 0);
 figure(1),subplot(3,1,1),imshow(original),title('Model');
 subplot(3,1,2),imshow(distorted),title('Scene');
 subplot(3,1,3),imshow(registered),title('Registered Scene');
--- genetic_function.m
+++ genetic_function.m
@@ -10,7 +10,7 @@ distorted = strcat(targetFolder, '\', distorted);
 distorted = imread(distorted);
 
 % Setup transformations
-scale = 0; %x(1); % % of original image size
+scale = 1; %x(1); % % of original image size
 theta = x(1); % degree rotation counterclockwise
 xTrans = 0; %x(3); %x translation defined in pixels
 yTrans = 0; %x(4); %y translation defined in pixels
@@ -19,10 +19,10 @@ yTrans = 0; %x(4); %y translation defined in pixels
 % [distorted, sample, xdata,ydata] = imretrieve(distorted, xTrans, yTrans, scale, theta);
 distorted = im_rst(distorted, 1, theta, 0, 0);
 
-%Measure similarity of images based on MOMI
-similarityMeasure=momi(original, distorted, 'Normalized');
+%Measure similarity of images based on MI
+similarityMeasure=mi(original, distorted);
 
-%Singe GA toolbox needs to minimize, we manipulate MOMI to achieve this
+%Singe GA toolbox needs to minimize, we manipulate MI to achieve this
 similarityMeasure=-similarityMeasure;
 
 end
